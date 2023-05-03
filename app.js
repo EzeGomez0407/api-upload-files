c7bf3f0003fa6649284c2fe6c05ebcf47affd21a
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-05-03 17:37:16 -0300

    modified port and add .env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3002;
+const { PORT } = process.env;
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
